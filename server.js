562176d7bed73582804785a00d4d6622bbae7b90
davidpraxedes2 <davidpraxedes2@users.noreply.github.com>
2026-01-06 00:39:21 -0300

    Reimportar db-helper e corrigir rota /api/products/:id para usar PostgreSQL

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const db = require('./db-helper');
 require('dotenv').config();
 
 const app = express();
